c757625364cb849d01c5eba034d76b65f9117c5d
DIY электроника <private.property240702@gmail.com>
2024-11-12 13:10:21 +0300

    Create age.ino

---
+++ examples/age/age.ino
@@ -0,0 +1,10 @@
+#include <DE_Years.h>
+
+Years Years;
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  Serial.println(Years.age(1990, 2024, WAS);  //год рождения, текущий год, время дня рождения (было или будет в году)
+}
